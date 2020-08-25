086a753432e6ec5a65566e5f21101d8403509614
Jack Weickert <jweicker@iu.edu>
2020-08-24 23:18:48 -0400

    Updates

---
+++ precache-manifest.5d52f764f393ef992f81b67ae8d7a69c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c5bd9609195a44c5173a839e2abbd0",
+    "url": "/ToDoReactApp/index.html"
+  },
+  {
+    "revision": "c8e659ff245cf71d750d",
+    "url": "/ToDoReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "156d10d77280861e7e09",
+    "url": "/ToDoReactApp/static/js/2.22923153.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoReactApp/static/js/2.22923153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e659ff245cf71d750d",
+    "url": "/ToDoReactApp/static/js/main.9a031a4d.chunk.js"
+  },
+  {
+    "revision": "f7be110c3a352d82b1a7",
+    "url": "/ToDoReactApp/static/js/runtime-main.d9b2c1da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDoReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
